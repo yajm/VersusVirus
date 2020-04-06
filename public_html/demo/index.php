e69d586e404168c324b9bbcbf71546dbcd72dc45
Yannick Mueller <muelleya@student.ethz.ch>
2020-04-06 09:36:24 +0200

    consult

--- public_html/demo/index.php
+++ public_html/demo/index.php
@@ -17,27 +17,27 @@ if(isset($_POST['submit'])){
     $emailErr = "Email is required";
   } 
   if (!empty($_POST["email"]) ) {
-  	include '../../engine/connection.php';
-  	$code = generateRandomString();
-  	$notes = "This is a demo account";
-  	$to = $_POST['email'];
-  	$name = $_POST['name'];
+    include '../../engine/connection.php';
+    $code = generateRandomString();
+    $notes = "This is a demo account";
+    $to = $_POST['email'];
+    $name = $_POST['name'];
 
-  	$sql = mysqli_query($conn, "INSERT INTO invite_link (code, hotline_id, assistant_id, doctor_id, hospital_id) VALUES ('$code', 1, 1, 1, 1)");
+    $sql = mysqli_query($conn, "INSERT INTO invite_link (code, hotline_id, assistant_id, doctor_id, hospital_id) VALUES ('$code', 1, 1, 1, 1)");
 
-  	$sql = mysqli_query($conn, "INSERT INTO patient (name, email, invitelink, helpline_notes) VALUES ('$name', '$to', '$code', '$notes')");
+    $sql = mysqli_query($conn, "INSERT INTO patient (name, email, invitelink, helpline_notes) VALUES ('$name', '$to', '$code', '$notes')");
 
      
     $from = "info@consultnow.ch";
     $subject = "DemoAccount - ConsultNow";
-    $message = "<html><body>Sehr geehrte/r " . $name . ",<br><br>Vielen Dank f&uuml;r Ihre Registration auf ConsultNow! <br><br>Sie sind fast mit Ihrem Arzt verbunden! Bitte klicken Sie auf den folgenden Link, um in minutenschnelle Ihre pers&ouml;nliche Konsultation anzutreten.<br><br><a href='https://student-uzh.ch/invite/?code=" . $code . "'>student-uzh.ch/invite/?code=" . $code . "</a><br><br>Alternativ k&ouml;nnen Sie direkt &uuml;ber unsere Webseite einloggen mit dem Code: " . $code . "<br><br>Wir w&uuml;nschen Ihnen beste Gesundheit.<br><br>Mit freundlichen Gr&uuml;ssen,<br>
+    $message = "<html><body>Sehr geehrte/r " . $name . ",<br><br>Vielen Dank f&uuml;r Ihre Registration auf ConsultNow! <br><br>Sie sind fast mit Ihrem Arzt verbunden! Bitte klicken Sie auf den folgenden Link, um in minutenschnelle Ihre pers&ouml;nliche Konsultation anzutreten.<br><br><a href='https://consultnow.ch/invite/?code=" . $code . "'>consultnow.ch/invite/?code=" . $code . "</a><br><br>Alternativ k&ouml;nnen Sie direkt &uuml;ber unsere Webseite einloggen mit dem Code: " . $code . "<br><br>Wir w&uuml;nschen Ihnen beste Gesundheit.<br><br>Mit freundlichen Gr&uuml;ssen,<br>
 Ihr ConsultNow Team</body></html>";
     $headers = "From: $from\r\n";
     $headers .= "Content-type: text/html\r\n";
     mail($to,$subject,$message,$headers);
 
     header('Location: /thanks');
-	}
+  }
 }
 ?>
 
@@ -58,16 +58,16 @@ Ihr ConsultNow Team</body></html>";
 <div class="pageCenter" style="background-color:#FFFFFFE0; height = 80%">
   <div style="padding-left:50px; padding-right:50px; padding-top:50px; padding-bottom:50px">
     <h1>Demo - For Patient</h1>
-    	<form method="post" action="#">
-		<input class="textfield" type="text" name="name" id="name" value="" placeholder="Name" />
-		<br>
-		<br>
-		<input class="textfield" type="text" name="email" id="email" value="" placeholder="E-Mail" />
-		<span class="error"> <?php echo $emailErr;?></span>
-		<br>
-		<br>
-		<button class="button" type="submit" name="submit">Senden</button>
-	</form>
+      <form method="post" action="#">
+    <input class="textfield" type="text" name="name" id="name" value="" placeholder="Name" />
+    <br>
+    <br>
+    <input class="textfield" type="text" name="email" id="email" value="" placeholder="E-Mail" />
+    <span class="error"> <?php echo $emailErr;?></span>
+    <br>
+    <br>
+    <button class="button" type="submit" name="submit">Senden</button>
+  </form>
   </div>
 </div>
 
